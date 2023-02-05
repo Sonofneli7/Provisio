d8feae927e7994b51aea151a269922839de644e0
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2023-02-04 22:32:38 -0500

    Update HotelControllerServlet.java
    newly added

--- src/main/java/hotel/jdbc/HotelControllerServlet.java
+++ src/main/java/hotel/jdbc/HotelControllerServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import javax.sql.rowset.serial.SerialException;
 
-import com.apple.laf.AquaGroupBorder;
+
 
 import hotel.jdbc.HotelDbUtil;
 
@@ -24,14 +25,18 @@ public class HotelControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     private HotelDbUtil hotelDbUtil;
-//Conn pool / Datasource
+    
+    //Conn pool / Datasource
+    
     @Resource(name="jdbc/provisio")
     private DataSource dataSource;
     
 	@Override
 	public void init() throws ServletException {
 		super.init();
+		
 		//create our hotel db util ... and pass in con pool/ datasource object
+		
 	try {
         hotelDbUtil = new HotelDbUtil(dataSource);
 
@@ -59,16 +64,22 @@ public class HotelControllerServlet extends HttpServlet {
        // throw exceptions to 1 central calling location to handle at once
        // Modifying the signature for listHotels
        
+		 try{
+		        // list the hotels... in MVC fashion
+			// get hotels from db util 
+		        List<Hotel> hotels = HotelDbUtil.getHotels();
 
-        // get hotels from db util 
-        List<Hotel> hotels = HotelDbUtil.getHotels();
-
-        // add hotels to the request
-        request.setAttribute("HOTEL_LIST", hotels);  // set attribute Name and actual object/value
-       
-        // send to JSP page (view); send data over to jsp page
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/list-hotels.jsp");
-        dispatcher.forward(request, response);
+		        // add hotels to the request
+		        request.setAttribute("HOTEL_LIST", hotels);  // set attribute Name and actual object/value
+		       
+		        // send to JSP page (view); send data over to jsp page
+		        RequestDispatcher dispatcher = request.getRequestDispatcher("/list-hotels.jsp");
+		        dispatcher.forward(request, response);
+		       }
+		       catch(Exception exc){
+		        throw new ServletException(exc);
+		       }
+        
 
 	}
 
